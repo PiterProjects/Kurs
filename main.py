59a40b07f5ec9514783f30028e24cd6cea68c3e5
PiterProjects <piotrekcvx77@gmail.com>
2023-08-09 11:52:01 +0200

    zmiana z uzytkownikiem

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-x = 10
-print(x)
+list=[]
+n = int(input("Podaj ilosc wynikow: "))
+for i in range(0,n):
+    liczba1=int(input("Podaj liczbe: "))
+    list.append(liczba1)
 
-print("Witaj przybyszu. ")
+print(list)
